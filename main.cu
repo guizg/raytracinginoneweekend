095043d84683462195cad9f085864f440abeb82d
guizg <guizgraicer@gmail.com>
2019-06-02 22:43:07 -0300

    ds

--- main.cu
+++ main.cu
@@ -38,7 +38,7 @@ __global__ void render(vec3 *fb, int max_x, int max_y, hitable **world){
     if (threadIdx.x == 0 && blockIdx.x == 0) {
         *(list) = new sphere(vec3(0,0,-1), 0.5);
         *(list+1) = new sphere(vec3(0,-100.5,-1), 100);
-        *(list+2) = new sphere(vec3(1, 0,-1), 0.7);
+        *(list+2) = new sphere(vec3(1, 0,-1), 0.5);
         *world    = new hitable_list(list,3);
     }
 }
